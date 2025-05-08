3c83bdfebb451d62d114aacced72c25637a2dfd7
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-05-08 10:04:07 -0700

    Changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,30 @@
 return {
 	'nvim-telescope/telescope.nvim', 
-	lazy  =false,
+	lazy = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
 	},
 	config = function()
-
 		local builtin = require("telescope.builtin")
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
+		
 		local telescope = require("telescope")
 		telescope.setup({
+			extensions = {
+				fzf = {
+					fuzzy = true,                   -- false will only do exact matching
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",       -- "smart_case" | "ignore_case" | "respect_case"
+				}
+			}
 		})
+		
+		-- Load the fzf extension
+		telescope.load_extension('fzf')
 	end	
 }
