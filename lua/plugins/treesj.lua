b15bfa1c04201e8a016ecc6392193e7abc66409f
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-05-20 11:19:50 -0700

    Changes

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,12 +1,12 @@
 return {
-  'Wansmer/treesj',
-  keys = {
-      "<leader>st", "<cmd>TSJToggle<cr>", desc = "Toggle structure pack/unpack",
-      "<leader>sp", "<cmd>TSJJoin<cr>", desc = "Pack structure into single line",
-      "<leader>su", "<cmd>TSJSplit<cr>", desc = "Unpack structure over line(s)",
-  },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
-  config = function()
-    require('treesj').setup({})
-  end,
+    "Wansmer/treesj",
+    keys = {
+        { "<leader>mt", "<cmd>TSJToggle<cr>", desc = "Toggle structure pack/unpack" },
+        { "<leader>mp", "<cmd>TSJJoin<cr>", desc = "Pack structure into single line" },
+        { "<leader>mu", "<cmd>TSJSplit<cr>", desc = "Unpack structure over line(s)" },
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+    config = function()
+        require("treesj").setup({})
+    end,
 }
