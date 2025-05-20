a279c5c727976d4036ded2f28b8d2680e3027da7
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-05-20 11:13:16 -0700

    Changes

--- lua/plugins/treesitter_context.lua
+++ lua/plugins/treesitter_context.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
         require("treesitter-context").setup({
-            enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+            enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
             multiwindow = false, -- Enable multiwindow support.
             max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
             min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
