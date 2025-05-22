4abd82fda72371e74fafba529c27aef2e3cc0e0a
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-05-21 21:20:22 -0700

    Telescope mapping improvements

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,15 +9,8 @@ return {
     config = function()
         local telescope = require("telescope")
         local actions = require("telescope.actions")
-        local transform_mod = require("telescope.actions.mt").transform_mod
         local trouble = require("trouble")
         local trouble_telescope = require("trouble.sources.telescope")
-
-        local custom_actions = transform_mod({
-            open_trouble_qflist = function(prompt_bufnr)
-                trouble.toggle("quickfix")
-            end,
-        })
 
         telescope.setup({
             defaults = {
@@ -26,15 +19,6 @@ return {
                         width = { padding = 0 },
                         height = { padding = 0 },
                         preview_width = 0.5,
-                    },
-                },
-
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                        ["<C-j>"] = actions.move_selection_next, -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-                        ["<C-t>"] = trouble_telescope.open,
                     },
                 },
             },
