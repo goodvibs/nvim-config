c8ba458755bf9290bd488f6d29d6e88a33dd44ca
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-05-22 13:29:19 -0700

    Install all parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "all" },
+            sync_install = true,
+            auto_install = true,
+            ignore_install = {},
+            modules = {}
+        }
+    end,
 }
