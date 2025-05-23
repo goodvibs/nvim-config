00d479b6d74bcf78c6c87de7f70628c79e038270
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-05-23 12:14:01 -0700

    Further improvements to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -24,26 +24,31 @@ return {
                 a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
+                z = { bg = colors.bg, colors.fg }
             },
             insert = {
                 a = { bg = colors.green, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
+                z = { bg = colors.bg, colors.fg }
             },
             visual = {
                 a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
+                z = { bg = colors.bg, colors.fg }
             },
             command = {
                 a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
+                z = { bg = colors.bg, colors.fg }
             },
             replace = {
                 a = { bg = colors.red, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
+                z = { bg = colors.bg, colors.fg }
             },
             inactive = {
                 a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
@@ -72,7 +77,6 @@ return {
                         color = { fg = "#ff9e64" },
                     },
                     { "lsp_status" },
-                    { "filetype" },
                 },
                 lualine_y = {}
             },
