251947f07776f8c68ad92061febdd9b8e1336ffc
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-05-29 14:24:21 -0500

    Maybe live grep will search hidden files too now?

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,7 +22,10 @@ return {
             pickers = {
                 find_files = {
                     hidden = true,
-                }
+                },
+                live_grep = {
+                    additional_args = { "--hidden" },
+                },
             },
             extensions = {
                 fzf = {
