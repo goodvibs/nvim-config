3b0051e2471e480416e2eb6829cbe90aab3a9e16
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-05-29 18:02:38 -0500

    Improved UX for autoformat toggle

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,9 +1,11 @@
 vim.api.nvim_create_user_command("FormatDisable", function(args)
     if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
+        -- formatdisable! will disable formatting just for this buffer
         vim.b.disable_autoformat = true
+        vim.api.nvim_echo({ { "Autoformat disabled for the current buffer" }, }, true, {})
     else
         vim.g.disable_autoformat = true
+        vim.api.nvim_echo({ { "Autoformat disabled globally" }, }, true, {})
     end
 end, {
     desc = "Disable autoformat-on-save",
@@ -12,6 +14,7 @@ end, {
 vim.api.nvim_create_user_command("FormatEnable", function()
     vim.b.disable_autoformat = false
     vim.g.disable_autoformat = false
+    vim.api.nvim_echo({ { "Autoformat enabled globally" }, }, true, {})
 end, {
-    desc = "Re-enable autoformat-on-save",
+    desc = "Enable autoformat-on-save",
 })
