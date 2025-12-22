b30a0f6f6233df5dafb553291ce77fd46783b24d
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-12-22 14:59:11 -0800

    Lazydev

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,26 @@
+if not vim.env.VIMRUNTIME then
+    local runtime_paths = {
+        "/opt/homebrew/opt/neovim/share/nvim/runtime",
+        "/usr/local/opt/neovim/share/nvim/runtime",
+    }
+    for _, path in ipairs(runtime_paths) do
+        if vim.fn.isdirectory(path) == 1 then
+            vim.env.VIMRUNTIME = path
+            break
+        end
+    end
+end
+
+return {
+    "folke/lazydev.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        library = {
+            vim.fn.stdpath("config"),
+        },
+        integrations = {
+            lspconfig = true,
+        },
+    },
+}
