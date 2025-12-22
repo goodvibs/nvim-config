092c4102093a4c2531f6c0f9a4bec4a0efeea712
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-12-22 10:27:06 -0800

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -49,17 +49,6 @@ return {
 
             ignore_install = {},
             highlight = {
-                enable = true,
-
-                disable = function(lang, buf)
-
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-
-
                 additional_vim_regex_highlighting = false,
             },
             indent = {
