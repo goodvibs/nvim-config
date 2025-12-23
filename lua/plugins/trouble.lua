00a12056d80058c8490f2934bd65abfa1336ef1d
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-12-22 19:09:03 -0800

    Remove unnecessary

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,7 +5,5 @@ return {
     keys = {
         { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>",              desc = "Trouble workspace diagnostics" },
         { "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Trouble buffer diagnostics" },
-        { "<leader>xq", "<cmd>Trouble qflist toggle<CR>",                   desc = "Trouble quickfix list" },
-        { "<leader>xl", "<cmd>Trouble loclist toggle<CR>",                  desc = "Trouble location list" },
     },
 }
