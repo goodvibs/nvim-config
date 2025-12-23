5159f9e6345d87eb239188bc2621026697b6ddba
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-12-22 20:23:29 -0800

    Simplify

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -5,16 +5,8 @@ return {
         local surround = require("mini.surround")
         surround.setup({
             mappings = {
-                add = 'msa',            -- Add surrounding in Normal and Visual modes
-                delete = 'msd',         -- Delete surrounding
-                find = 'msf',           -- Find surrounding (to the right)
-                find_left = 'msF',      -- Find surrounding (to the left)
-                highlight = 'msh',      -- Highlight surrounding
-                replace = 'msr',        -- Replace surrounding
-                update_n_lines = 'msn', -- Update `n_lines`
-
-                suffix_last = 'l',      -- Suffix to search with "prev" method
-                suffix_next = 'n',      -- Suffix to search with "next" method
+                add = 'msa',
+                delete = 'msd',
             },
         })
     end
